6a674ceb048ad961280a6dfd21026a165ab9203c
ocastelblanco <ocastelblanco@gmail.com>
2013-02-21 19:53:26 -0500

    Ajuste de Lightbox
    Ajuste de funcionalidad de la lightbox, de estilos de los elementos.
    Pendiente de finalizar el footer del lightbox, con el vínculo Abrir en
    nueva pantalla.

--- publicacion/js/scripts.js
+++ publicacion/js/scripts.js
@@ -38,43 +38,64 @@ $(function() {
 			$(this).parent().children('#indicador').css('border-bottom', '0.5em solid white');
 		});
     });
-	$('#contenido .thumb a').each(function(e) {
+	$('#contenido .thumb a').click(function(e) {
+		e.preventDefault();
 		var obj = $(this);
 		var nombre = $(obj).attr('title');
 		$.getJSON('portfolio/'+nombre+'/desc.json', function(datos) {
 			var titulo = '<div id="titulo">'+$(obj).children('#texto').children('#titulo').html()+'</div>';
-			var cliente = '<div id="cliente">'+$(obj).children('#texto').children('#cliente').html()+'</div>';
-			var fecha = '<div id="fecha">'+$(obj).children('#texto').children('#fecha').html()+'</div>';
-			var tipo = '<div id="tipo">'+$(obj).children('#texto').children('#tipo').html()+'</div>';
+			var cliente = '<div id="cliente"><span>Client: </span>'+$(obj).children('#texto').children('#cliente').html()+'</div>';
+			var fecha = '<div id="fecha"><span>Date: </span>'+$(obj).children('#texto').children('#fecha').html()+'</div>';
+			var tipo = '<div id="tipo"><span>Developed in: </span>'+$(obj).children('#texto').children('#tipo').html()+'</div>';
+			var desc = '<div id="desc"><span>Description: </span>'+datos.desc+'</div>';
+			var rol = '<div id="rol"><span>Role: </span>'+datos.rol+'</div>';
+			var sep = '<div id="sep"></div>';
 			$('#cajas').append('<div id="caja-'+nombre+'" class="lightbox-block"><div id="contenido-'+nombre+'">');
 			$('#cajas').append('</div></div>');
 			$('#cajas').children('#caja-'+nombre).lightbox();
 			var contenido = $('#cajas').children('.lightbox').children('#caja-'+nombre).children('#contenido-'+nombre);
-			var sliderInicio = '<div id="portaSlider"><div id="slider'+nombre+'">';
+			var sliderInicio = '<div id="portaSlider"><div id="slider">';
 			var sliderInto = '';
 			for (var i = 0;i<datos.screens;i++) {
 				sliderInto += '<img src="portfolio/'+nombre+'/Screen'+dosDigitos(i+1)+'.jpg">';
 			}
 			var sliderOut = '</div></div>';
 			var slider = sliderInicio+sliderInto+sliderOut;
-			$(contenido).html(titulo+slider+'<div id="desc">'+datos.desc+'</div>'+tipo+fecha+cliente);
+			$(contenido).html(titulo+slider+sep+rol+desc+tipo+fecha+cliente);
 		}).error(function(){
 			$(contenido).html("A communication error happened. Try reloading the page to see this content.");
 		}).complete(function() {
-			var demora = window.setTimeout(function() {
-				$('#slider'+nombre).each(function(index, element) {
-					if (!$(this).hasClass('orbit')) {
-						$(this).orbit();
-					}   
+			var bkg = $('#caja-'+nombre).parent('.lightbox').css('background');
+			$('#cajas .lightbox').css('background', 'rgba(0, 0, 0, 0)');
+			$('#cajas .lightbox').show('scale', {percent: 100}, 500, function() {
+				$('#cajas #slider').orbit();
+				$(this).css('background', bkg);
+				$('div.slider-nav span.left').addClass('navLeft');
+				$('div.slider-nav span.right').addClass('navRight');
+				$('div.slider-nav span.left').mouseenter(function(e) {
+                    $(this).switchClass('navLeft', 'navLeft0');
                 });
-			}, 10000);
+				$('div.slider-nav span.left').mouseleave(function(e) {
+                    $(this).switchClass('navLeft0', 'navLeft');
+                });
+				$('div.slider-nav span.right').mouseenter(function(e) {
+                    $(this).switchClass('navRight', 'navRight0');
+                });
+				$('div.slider-nav span.right').mouseleave(function(e) {
+                    $(this).switchClass('navRight0', 'navRight');
+                });
+			});
+			$('#cajas .lightbox, .lightbox #close').click(function(e) {
+				$('#caja-'+nombre).parent('.lightbox').css('background', 'rgba(0, 0, 0, 0)');
+                $(this).hide('scale', {percent: 0}, 500, function(){
+					$('#cajas').children().remove();
+				});
+            });
+			$('#caja-'+nombre).click(function(e) {
+                e.stopPropagation();
+            });
 		});
     });
-	$('#contenido .thumb a').click(function(e) {
-		var nombre = $(this).attr('title');
-		$('#caja-'+nombre).parent('.lightbox').show();
-        return false;
-	});
 	/* Funciones generales */
 	function ocultaTextos(obj) {
 		$(obj).children('#cliente').hide();
