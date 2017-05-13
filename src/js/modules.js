e7042a6028ab8c31ee6722e826d8aa17aee92991
Oliver Castelblanco <ocastelblanco@gmail.com>
2017-05-13 00:14:10 +0000

    Animación scroll

--- src/js/modules.js
+++ src/js/modules.js
@@ -26,7 +26,7 @@ app.controller('contenido', ['$rootScope',function($rootScope){
     raiz.rutaHeader = 'views/navbar.html';
     raiz.rutaBody = 'views/portfolio.html';
     raiz.rutaLogoPreloader = 'views/logoAnimado.html';
-    raiz.cargado = true;//false;
+    raiz.cargado = false;
     $rootScope.$on('finPrecarga',function(e,a){
         raiz.cargado = true;
     });
@@ -58,3 +58,39 @@ app.controller('portfolio', [function(){
     console.log('portfolio');
     var raiz = this;
 }]);
+// Directivas
+app.directive('scrollAbajo', function ($document,$timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var doc = angular.element($document)[0].body;
+            var margen = window.getComputedStyle(element[0], null).getPropertyValue('margin-top');
+            margen = parseInt(margen,10);
+            var margenNegativa = window.getComputedStyle(element[0].children[0], null).getPropertyValue('margin-top');
+            margenNegativa = parseInt(margenNegativa, 10);
+            var alturaBarra,top,margenInf,margenSup,navbar,fondoNavbar,opacidad;
+            $timeout(function(){
+                navbar = angular.element(document.getElementById('navbar'))[0];
+                fondoNavbar = navbar.children[0];
+                opacidad = parseInt(window.getComputedStyle(fondoNavbar, null).getPropertyValue('opacity'),10);
+                alturaBarra = navbar.clientHeight;
+                top = margen-alturaBarra+margenNegativa;
+                margenInf = top+margenNegativa;
+                margenSup = margen+margenNegativa;
+                navbar,fondoNavbar,opacidad;
+            },100);
+            $document.bind("scroll", function () {
+                var opacidadActual = 1-(((margenSup-margenInf)-(doc.scrollTop-margenInf))/(margenSup-margenInf));
+                if (opacidadActual > 1) {
+                    opacidadActual = 1;
+                } else if (opacidadActual < 0) {
+                    opacidadActual = 0;
+                }
+                if (opacidad == 0) {
+                    angular.element(fondoNavbar).css('opacity',opacidadActual);
+                }
+                angular.element(navbar).css('box-shadow', '0px 0px 1px 0px rgba(0,0,0,'+opacidadActual+')');
+            });
+        }
+    };
+});
