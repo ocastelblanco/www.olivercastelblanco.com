4af836b1620457ab7b69d53a4f6373310d4afd4c
ocastelblanco <ocastelblanco@gmail.com>
2022-06-09 17:22:30 -0500

    Website v2 blank start

--- node_modules/grunt-ftp-push/files/nested/another/sample.js
+++
@@ -1,2 +0,0 @@
-var num = 42;
-alert(num);
--- node_modules/grunt-ftp-push/tasks/cache.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var fs = require('fs');
-
-var cachePath = path.join(__dirname, '_filecache.json');
-
-module.exports = {
-  get: function () { return JSON.parse(fs.readFileSync(cachePath, 'utf-8')); },
-  set: function (cache) { fs.writeFileSync(cachePath, JSON.stringify(cache)); }
-};
