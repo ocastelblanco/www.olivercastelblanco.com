7299ec25bd908956f4a46faca82726030a7b6854
ocastelblanco <ocastelblanco@gmail.com>
2022-06-29 16:23:40 -0500

    chore: PWA implementation

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/www.olivercastelblanco.com'),
+      dir: require('path').join(__dirname, './coverage/ocastelblanco'),
       subdir: '.',
       reporters: [
         { type: 'html' },
