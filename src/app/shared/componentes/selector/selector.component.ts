fe4e48a95bb5fdd2b0fcbefadccbf152d882c5ea
Oliver Castelblanco <ocastelblanco@gmail.com>
2024-02-27 18:05:38 -0500

    fix: language selector elements

--- src/app/shared/componentes/selector/selector.component.ts
+++ src/app/shared/componentes/selector/selector.component.ts
@@ -32,5 +32,22 @@ export class SelectorComponent implements OnInit {
     const iconoComponente: HTMLElement = iconoComponenteRef.location.nativeElement;
     this.renderer.addClass(iconoComponente, 'oca-icono-selector');
     this.renderer.appendChild(selector, iconoComponente);
+    // Crea enlaces con iconos SVG externos
+    const listaSVG: HTMLCollection = this.elemento.getElementsByClassName('svg');
+    for (let i: number = 0; i < listaSVG.length; i++) {
+      const svg: HTMLElement = listaSVG.item(i) as HTMLElement;
+      const texto: any = this.renderer.createText(svg.innerHTML);
+      const span: HTMLElement = this.renderer.createElement('span');
+      this.renderer.addClass(span, 'texto');
+      this.renderer.appendChild(span, texto);
+      svg.innerHTML = '';
+      const fuente: string = svg.getAttribute('fuente') as string;
+      const nombre: string = svg.getAttribute('svg') as string;
+      const imagen: any = this.renderer.createElement('img');
+      this.renderer.addClass(imagen, 'img-svg');
+      this.renderer.setAttribute(imagen, 'src', `assets/${fuente}/${nombre}.svg`);
+      this.renderer.appendChild(svg, imagen);
+      this.renderer.appendChild(svg, span);
+    }
   }
 }
