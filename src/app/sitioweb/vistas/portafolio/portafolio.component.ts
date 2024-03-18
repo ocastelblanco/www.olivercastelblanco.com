de39883f3285b517570f4518882230cd8d191b94
Oliver Castelblanco <ocastelblanco@gmail.com>
2024-03-18 12:24:49 -0500

    feat: overlay captures controls

--- src/app/sitioweb/vistas/portafolio/portafolio.component.ts
+++ src/app/sitioweb/vistas/portafolio/portafolio.component.ts
@@ -13,12 +13,16 @@ export class PortafolioComponent {
   overlayVisible: boolean = false;
   overlayData: Captura = { img: '', txt: '' };
   propCapOver: string | null = null;
+  numProyectoOverlay: number = 0;
+  numCapturaOverlay: number = 0;
   constructor(private func: FuncionesService, private data: DataService) {
     effect(() => this.idioma = this.func.idioma());
     this.data.getInterfaz().subscribe((_interfaz: any) => this.proyectos = _interfaz.contenidos.portafolio);
   }
-  abreCaptura(proyecto: Captura, ev: MouseEvent): void {
-    this.overlayData = proyecto;
+  abreCaptura(numProy: number, numCap: number): void {
+    this.numProyectoOverlay = numProy;
+    this.numCapturaOverlay = numCap;
+    this.overlayData = this.proyectos[this.numProyectoOverlay].capturas[this.numCapturaOverlay];
     this.overlayVisible = true;
   }
   propCaptura(ev: Event): void {
@@ -27,5 +31,12 @@ export class PortafolioComponent {
     if (prop < 0.8) this.propCapOver = 'vertical';
     if (prop >= 0.8 && prop <= 1.25) this.propCapOver = 'estandar';
     if (prop > 1.25) this.propCapOver = 'horizontal';
+  }
+  overlayCambiaCaptura(dir: -1 | 1): void {
+    const fin: number = this.proyectos[this.numProyectoOverlay].capturas.length - 1;
+    if (this.numCapturaOverlay == 0 && dir == -1) this.numCapturaOverlay = fin + 1;
+    if (this.numCapturaOverlay == fin && dir == 1) this.numCapturaOverlay = -1;
+    this.numCapturaOverlay += dir;
+    this.overlayData = this.proyectos[this.numProyectoOverlay].capturas[this.numCapturaOverlay];
   }
 }
